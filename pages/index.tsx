3c8132ac0f2eb8d6ba4504bc003ac8502f1d3e0f
Maanav Khaitan <maanav@berkeley.edu>
2023-03-25 23:31:09 -0700

    Added &apos

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Hi, I'm Maanav.</title>
+        <title>{"Hi, I'm Maanav."}</title>
         <meta name="description" content="Maanav Khaitan" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/ja1.jpg" />
@@ -18,15 +18,15 @@ export default function Home() {
           <div className="lg:grid lg:grid-rows-2 lg:grid-cols-3 grid py-24 sm:py-36 sm:px-36 gap-12">
           <div className="container">
             <img src="/ja1.jpg" className="h-64 w-64" />
-            <h2 className="centered text-5xl text-start font-serif">Hi, I'm Maanav.</h2>
+            <h2 className="centered text-5xl text-start font-serif">{"Hi, I'm Maanav."}</h2>
           </div>
           <div className="container">
             <img src="/ja2.jpeg" className="h-64 w-64" />
-            <h2 className="centered text-xl text-start font-serif">I'm studying computer science and business at Berkeley.</h2>
+            <h2 className="centered text-xl text-start font-serif">{"I'm studying computer science and business at Berkeley."}</h2>
           </div>
           <div className="container">
             <img src="/ja6.jpeg" className="h-64 w-64" />
-            <h2 className="centered text-xl text-start font-serif">I'm currently working on app-specific blockchains at <a target="_blank" href="https://caldera.xyz" className="underline">Caldera.</a></h2>
+            <h2 className="centered text-xl text-start font-serif">I&apos;'m currently working on app-specific blockchains at <a target="_blank" href="https://caldera.xyz" className="underline">Caldera.</a></h2>
           </div>
           <div className="container">
             <img src="/ja10.webp" className="h-64 w-64" />
@@ -34,7 +34,7 @@ export default function Home() {
           </div>
           <div className="container">
             <img src="/ja11.jpeg" className="h-64 w-64" />
-            <h2 className="centered text-xl text-start font-serif">I love making <a target="_blank" href="https://caldera.xyz" className="underline">fun things</a> with friends, creating <a target="_blank" href="https://caldera.xyz" className="underline">art</a>, and I'm trying to <a target="_blank" href="https://caldera.xyz" className="underline">read more</a>.</h2>
+            <h2 className="centered text-xl text-start font-serif">I love making <a target="_blank" href="https://caldera.xyz" className="underline">fun things</a> with friends, creating <a target="_blank" href="https://caldera.xyz" className="underline">art</a>, and I&apos;'m trying to <a target="_blank" href="https://caldera.xyz" className="underline">read more</a>.</h2>
           </div>
           <div className="container">
             <img src="/ja8.jpeg" className="h-64 w-64" />
