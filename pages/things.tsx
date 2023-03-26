b72245c6f3222ff066e824514f7a96e2d3c3f017
Maanav Khaitan <maanav@berkeley.edu>
2023-03-26 01:23:28 -0700

    Fixed &apos

--- pages/things.tsx
+++ pages/things.tsx
@@ -19,18 +19,18 @@ export default function Reading() {
         <div className="flex flex-col sm:flex-row gap-12 my-36 md:mx-36 mx-8">
         <img src="/ja4.jpeg" className="h-64 w-64" />
         <div>
-        <h2 className="text-5xl text-start font-serif">Things I've Built</h2>
-        <h2 className="text-md text-start font-serif mt-4">there's nothing more fun than being a buildooooor</h2>
+        <h2 className="text-5xl text-start font-serif">Things I&apos;ve Built</h2>
+        <h2 className="text-md text-start font-serif mt-4">there&apos;s nothing more fun than being a buildooooor</h2>
         <h2 className="text-xl mt-8 text-start font-serif text-zinc-700"><a target="_blank" href="https://ethglobal.com/showcase/traverse-5xy8p" className="underline">Traverse</a></h2>
         <h2 className="text-lg mt-1 text-start font-serif text-zinc-500">Automatically generate security reports for EVM smart contracts, using static analysis.</h2>
         <h2 className="text-xl mt-8 text-start font-serif text-zinc-700"><a target="_blank" href="https://www.covidplasma.in/" className="underline">Covidplasma.in</a></h2>
-        <h2 className="text-lg mt-1 text-start font-serif text-zinc-500">India's first online dashboard to connect COVID-19 patients with plasma donors. Connected 3500 patients and donors.</h2>
+        <h2 className="text-lg mt-1 text-start font-serif text-zinc-500">India&apos;s first online dashboard to connect COVID-19 patients with plasma donors. Connected 3500 patients and donors.</h2>
         <h2 className="text-xl mt-8 text-start font-serif text-zinc-700"><a target="_blank" href="https://ethglobal.com/showcase/lilac-waeur" className="underline">Lilac</a></h2>
         <h2 className="text-lg mt-1 text-start font-serif text-zinc-500">Improving crypto governance by enabling DAOs and protocols to reward their most active contributors with voting power.</h2>
         <h2 className="text-xl mt-8 text-start font-serif text-zinc-700"><a target="_blank" href="https://drive.google.com/file/d/1D5neg_swZAK4LKPEskaCmBLJ1oGcThP4/view" className="underline">Outlier-resistant Uniswap price oracle</a></h2>
-        <h2 className="text-lg mt-1 text-start font-serif text-zinc-500">Making Uniswap's TWAP price oracle more outlier-resistant by implementing a winsorized TWAP oracle from scratch.</h2>
+        <h2 className="text-lg mt-1 text-start font-serif text-zinc-500">Making Uniswap&apos;s TWAP price oracle more outlier-resistant by implementing a winsorized TWAP oracle from scratch.</h2>
         <h2 className="text-xl mt-8 text-start font-serif text-zinc-700"><a target="_blank" href="https://apoyoapp.wixsite.com/apoyo" className="underline">Apoyo</a></h2>
-        <h2 className="text-lg mt-1 text-start font-serif text-zinc-500">An iOS app to help Alzheimer's caregivers remind their loved ones of daily tasks and ensure their safety.</h2>
+        <h2 className="text-lg mt-1 text-start font-serif text-zinc-500">An iOS app to help Alzheimer&apos;s caregivers remind their loved ones of daily tasks and ensure their safety.</h2>
         <h2 className="text-xl mt-8 text-start font-serif text-zinc-700"><a target="_blank" href="https://apps.apple.com/us/app/beacon-safety/id1106393380" className="underline">Beacon Safety</a></h2>
         <h2 className="text-lg mt-1 text-start font-serif text-zinc-500">An iOS app to alert and send your location to your close contacts when unsafe. Received 2300 downloads.</h2>
         <h2 className="text-xl mt-8 text-start font-serif text-zinc-700"><a target="_blank" href="https://github.com/MaanavKhaitan/random-walk-music-generator" className="underline">Random Walk Music Generator</a></h2>
