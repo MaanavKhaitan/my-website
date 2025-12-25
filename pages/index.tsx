9b245dae6df537e76c33a344217e98c51620f479
Maanav Khaitan <me@maanavkhaitan.com>
2025-12-25 12:48:08 +0530

    links

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           </div>
           <div className="container">
             <img src="/ja2.jpeg" className="h-64 w-64" />
-            <h2 className="centered text-xl text-start font-serif">I&apos;m currently working on building high-performance systems and new markets in crypto.</h2>
+            <h2 className="centered text-xl text-start font-serif">I&apos;m currently working on high-performance systems and new markets in crypto.</h2>
           </div>
           <div className="container">
             <img src="/ja6.jpeg" className="h-64 w-64" />
